f831365bce847b64fa3c7533ee19777a22dfd8fd
srikartalluri <srikartalluri@berkeley.edu>
2024-05-02 02:26:13 -0700

    migrate to org

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -22,9 +22,13 @@ import {
 	ModalCloseButton,
   } from '@chakra-ui/react'
 
+
+
 function CanvasApp() {
 	const [width, setWidth] = React.useState(window.innerWidth);
   	const [height, setHeight] = React.useState(window.innerHeight);
+	
+	  const rootURL = "https://paintbin.github.io/paintbin";
 
 	// ref for the current canvas
     const [canvas, setCanvas] = useState<CanvasDraw | null>(null);
@@ -49,7 +53,7 @@ function CanvasApp() {
 		toast({
 			position: 'top',
 			title: 'Paint created at:',
-			description: "srikartalluri.github.io/paintbin/#/" + prom.id,
+			description: rootURL + "/#/" + prom.id,
 			status: 'success',
 			duration: 10000,
 			isClosable: true,
